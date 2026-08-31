51b9b95f73c77ffe244248eb8386f3857f31cca3
aarodriguezperez <albertoalejandro.rp@gmail.com>
2026-08-31 17:04:45 -0600

    Integración Práctica API Cocktails

--- practica_cocktails/functions.js
+++ practica_cocktails/functions.js
@@ -1,29 +1,10 @@
 console.log("Hello World Cocktails");
 
 async function obtenerCocteles() {
-
-    const response = await fetch(
-        "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
-    );
-
+    const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=c");
     const data = await response.json();
-
-    const cocteles = data.drinks.slice(0, 15);
-
-    const coctelesCompletos = [];
-
-    for (const coctel of cocteles) {
-
-        const responseDetalle = await fetch(
-            `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${coctel.idDrink}`
-        );
-
-        const dataDetalle = await responseDetalle.json();
-
-        coctelesCompletos.push(dataDetalle.drinks[0]);
-    }
-
-    return coctelesCompletos;
+    console.log('Cocteles:', data.drinks);
+    return data.drinks.slice(0, 15);
 }
 
 console.log(obtenerCocteles());
@@ -34,13 +15,13 @@ function pintarCocteles(cocteles) {
     console.log("Tarjetas HTML:", tarjetasHTML);
     cocteles.forEach(coctel => {
         tarjetasHTML += `
-        <div class="card">
-            <img src="${coctel.strDrinkThumb}" alt="${coctel.strDrink}">
-            <h3>${coctel.strDrink}</h3>
-            <p class="category">${coctel.strCategory}</p>
-            <p class="alcoholic">${coctel.strAlcoholic}</p>
-            <p class="glass">${coctel.strGlass}</p>
-        </div>
+            <div class="card">
+                <img src="${coctel.strDrinkThumb}" alt="${coctel.strDrink}">
+                <h3>${coctel.strDrink}</h3>
+                <p>${coctel.strCategory}</p>
+                <p>${coctel.strAlcoholic}</p>
+                <p>${coctel.strGlass}</p>
+            </div>
         `;
     });
     document.getElementById("main-container").innerHTML = tarjetasHTML;
